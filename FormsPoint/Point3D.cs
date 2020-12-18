361d955ff1253769504ea117c24fc877b5223232
MSLV <slavainhell@mail.ru>
2020-12-18 11:26:01 +0500

    Add project files.

---
+++ FormsPoint/Point3D.cs
@@ -0,0 +1,33 @@
+﻿using PointLib;
+using System;
+
+namespace FormsPoint
+{
+    [Serializable]
+    class Point3D:Point//класс Point3D наследный от Point
+    {
+        public int Z { get; set; }//объявление переменной 
+
+        public Point3D() : base()
+        {
+            Z = rnd.Next(10);//присвоение случайного значения z
+        }
+
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            Z = z;
+        }
+
+        public override double Metric()//метод измерения растояния от начала координат до точки
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        public override string ToString()//метод возвращающий положение координаты в виде строки
+        {
+            return string.Format($"({X} , {Y}, {Z})");
+        }
+
+
+    }
+}
---
+++ FormsPoint/PointForm.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using PointLib;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace FormsPoint
+{
+    public partial class PointForm : Form
+    {
+        private Point[] points = null;
+
+
+        public PointForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)//при нажатие на кнопку будет созданы 10 точек
+        {
+            points = new Point[10];
+
+            var rnd = new Random();
+
+            for (int i = 0; i < points.Length; i++)
+                points[i] = rnd.Next(3) % 2 == 0 ? new Point() : new Point3D();
+
+            listBox.DataSource = points;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)//сортировка массива точек
+        {
+            if (points == null)
+                return;
+
+            Array.Sort(points);
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)//сериализация массива точек
+        {
+            var dlg = new SaveFileDialog();//создаие формы и фильтр в форме по типам файлов
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs =
+                new FileStream(dlg.FileName, FileMode.Create, FileAccess.Write))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin"://сериализация в bin
+                        var bf = new BinaryFormatter();
+                        bf.Serialize(fs, points);
+                        break;
+                    case ".soap"://сериализация в soap
+                        var sf = new SoapFormatter();
+                        sf.Serialize(fs, points);
+                        break;
+                    case ".xml"://сериализация в xml
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        xf.Serialize(fs, points);
+                        break;
+                    case ".json"://сериализация в json
+                        var jf = new JsonSerializer();
+                        using (var w = new StreamWriter(fs))
+                            jf.Serialize(w, points);
+                        break;
+                }
+            }
+
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)//десериализация
+        {
+            var dlg = new OpenFileDialog();//создаие формы и фильтр в форме по типам файлов
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs =
+                new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin"://десериализация из bin
+                        var bf = new BinaryFormatter();
+                        points = (Point[])bf.Deserialize(fs);
+                        break;
+                    case ".soap"://десериализация из soap
+                        var sf = new SoapFormatter();
+                        points = (Point[])sf.Deserialize(fs);
+                        break;
+                    case ".xml"://десериализация из xml
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        points = (Point[])xf.Deserialize(fs);
+                        break;
+                    case ".json"://десериализация из json
+                        var jf = new JsonSerializer();
+                        using (var r = new StreamReader(fs))
+                            points = (Point[])jf.Deserialize(r, typeof(Point[]));
+                        break;
+                }
+            }
+
+        }
+    }
+}
